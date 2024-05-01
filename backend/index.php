81b4aa0cb5a1252b6c86f1e1e31f5bbca0a39bbe
GamerCleanVic <jonassd2000@gmail.com>
2024-04-30 21:40:01 -0300

    Vars and Types

--- backend/index.php
+++ backend/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+//Tipos
+$nome = "Cicrano"; //String
+$idade = 90; //Inteiro
+$dormindo = true; //Boolean
+$vazia = null; //Null
+$sobrenome = '';//Void
+echo "Nome: $nome<br>
+    Idade: $idade<br>
+    Dormindo: $dormindo<br>
+
+";
+echo 'Nula: ';var_dump($vazia);
+echo'<br>String vazia: ';var_dump($sobrenome);
