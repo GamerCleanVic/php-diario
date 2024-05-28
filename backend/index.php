0d0dd8ab40bfefa6720d8a75eda474ecdd1238cf
GamerCleanVic <jonassd2000@gmail.com>
2024-05-27 23:50:59 -0300

    sub string

--- backend/index.php
+++ backend/index.php
@@ -2,10 +2,7 @@
 //Módulo 3: Funções Nativas de Matemática
 require "../frontend/css/phpColors.php";
 
-echo"<h1 style='
-    text-align: center;
-'>TABUADA</h1>";
-
-$num1 = readline("Digite 1 nº inteiro: ");
-readline_add_history($num1);
-
+$nome = "Bonieky";
+$fullName = $nome;
+$fullName .= " Lacerda";
+echo substr($fullName, 0, 3);
