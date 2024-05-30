1c7d6f3da123026394c902f755767430698a2313
GamerCleanVic <jonassd2000@gmail.com>
2024-05-30 01:36:29 -0300

    multi req

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,5 @@
+<a href="./paginas/home.php">HOME</a>
+<a href="./paginas/sobre.php">SOBRE</a>
 <?php
-//Módulo 3: Funções Nativas de Matemática
-require "../frontend/css/phpColors.php";
-
-$nome = "Bonieky";
-$fullName = $nome;
-$fullName .= " Lacerda";
-echo substr($fullName, 0, 3);
+//Módulo 4: Introdução à HTTP Requests
+require('../frontend/css/phpColors.php');
--- frontend/css/phpColors.php
+++ frontend/css/phpColors.php
@@ -5,5 +5,14 @@ echo "
             background-color: #181818;
             color: #ccc;
         }
+	.btn1{
+	    background-color: lightgreen;
+	    color: darkgreen;
+	    padding: 5px;
+	    padding-left: 10px;
+	    padding-right: 10px;
+	    font-weight: bold;
+	    border-radius: 5px;
+	}
     </style>
 ";
