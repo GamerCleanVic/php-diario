42a2bdabeb28adb81ce1c63b71f59e07d5e22b2b
GamerCleanVic <jonassd2000@gmail.com>
2024-09-22 00:10:39 -0300

    fn paran opcional

--- backend/index.php
+++ backend/index.php
@@ -1,37 +1,38 @@
 <?php
-//Módulo 2: Array - Aula 1
+//Módulo 2: Funções - Tipagem de Parâmetros e Retornos #14
 require_once "header.php";
 
-// function somar($num1, $num2, $num3 = null){
-//     if($num3){
-//         return "Número1: $num1, Número2: $num2, Número3: $num3";
-//     }else{
-//         return "Número1: $num1, Número2: $num2";
-//     }
-// }
+function somar($num1, $num2, $num3 = null){
+    if($num3){
+        return "Número1: $num1, Número2: $num2, Numero3: $num3";
+    }else{
+        return "Número1: $num1, Número2: $num2";
+    }
 
-function verificaIdade($anoNascimento) {
-    $anoAtual = date('Y');
-    return $anoAtual - $anoNascimento;
 }
 
-function portaoDaBalada($anoNascimento, $acompanhado = false){
+// $result = somar(2, 3);
+// echo $result;
+
+function verificaIdade($anoNascimento){
+    $anoAtual = date("Y");
+    $idade = $anoAtual - $anoNascimento;
+    return $idade;
+}
+function portaoDeBalada($anoNascimento, $acompanhado = false){
     $idadeParaEntrarNaBalada = 18;
     $idade = verificaIdade($anoNascimento);
-
     if($idade >= $idadeParaEntrarNaBalada){
-        return "Pode entrar na balada!";
-    }elseif($idade < $idadeParaEntrarNaBalada && $acompanhado){
-        return "Pode entrar na balada com seu acompanhante!";
+        return 'Pode entrar na balada!';
+    }elseif($idade >= $idadeParaEntrarNaBalada && $acompanhado){
+        return 'Pode entrar na balada com seu acompanhante!';
+    }elseif(($idade == ($idadeParaEntrarNaBalada -1) && $acompanhado) || ($idade == ($idadeParaEntrarNaBalada -2) && $acompanhado)){
+        return 'Pode entrar na balada com seu acompanhante!';
     }else{
-        return "Você não pode entrar na balada!";
+        return 'Não pode entrar na balada!';
     }
 }
 
-
-echo "1994 sem acompanhante: ".portaoDaBalada(1994)."<br>";
-echo "1999 com acompanhante: ".portaoDaBalada(1999, true)."<br>";
-echo "2014 com acompanhante: ".portaoDaBalada(2014, true)."<br>";
-echo "2015 sem acompanhante: ".portaoDaBalada(2015, false)."<br>";
-echo "2011 sem acompanhante: ".portaoDaBalada(2011)."<br>";
-
+echo "1994 sem acompanhante: ".portaoDeBalada(1994)."<br>";
+echo "2007 com acompanhante: ".portaoDeBalada(2007, true)."<br>";
+echo "2014 com acompanhante: ".portaoDeBalada(2014, true)."<br>";
