fee7fc9fdc80887006d15eb5d27f957167bc72fe
GamerCleanVic <jonassd2000@gmail.com>
2024-10-24 19:40:12 -0300

    error msg

--- backend/dados.php
+++ backend/dados.php
@@ -1,9 +1,37 @@
 <?php
 require_once "header.php";
 
-if(!empty($_POST['login'] && !empty($_POST['password']))){
+if(!empty($_POST['login']) && !empty($_POST['password'])){
     $login = htmlspecialchars($_POST['login']);
     $password = htmlspecialchars($_POST['password']);
 
-    echo "Seu login é $login e sua senha é $password";
+    $mensagem = null;
+    if(($login == 'admin') && ($password == 'admin')){
+        $mensagem = "<br>Login efetuado com sucesso!";
+    }else{
+        $mensagem = "<br>Login ou senha inválida!";
+    }
+
+    //echo "O seu login é: $login e o se password é: $password.";
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="dados.php" method="POST">
+    <input type="text" name="login" placeholder="Login"><br>
+    <input type="password" name="password" placeholder="Senha"><br>
+    <input type="submit" value="Enviar">
+</form>
+    <?php
+        if(!empty($mensagem)){
+            echo $mensagem;
+        }
+    ?>
+</body>
+</html>
--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,5 @@
 <?php
-//Módulo 3: Lidando com Mensagens de Erro em Formulários #7
+//Módulo 3: Validação e Persistência de Formulários com o Operador de Coalescência Nula (Parte 1) #8
 require_once "header.php";
 //require_once "../frontend/components/item/forms/campanha.php";
-require_once "../frontend/components/item/forms/dados.php";
-
-?>
-<form action="dados.php" method="POST">
-    <input type="text" name="login" placeholder="Login"><br>
-    <input type="password" name="password" placeholder="Senha"><br>
-    <input type="submit" value="Enviar">
-</form>
+require "dados.php";
